ea14ef2dde2360b19d0a0794c4123b50c688421c
ameen <afor1m3en@gmail.com>
2022-11-19 16:06:56 +0530

    add:todo and tests

---
+++ todo.js
@@ -0,0 +1,32 @@
+const todoList = () => {
+  all = []
+  const add = (todoItem) => {
+    all.push(todoItem)
+  }
+  const markAsComplete = (index) => {
+    all[index].completed = true
+  }
+
+  const overdue = () => {
+   return all.filter((item)=>item.dueDate< today)
+  }
+
+  const dueToday = () => {
+     return all.filter((item)=>item.dueDate=== today)
+  }
+
+  const dueLater = () => {
+    return all.filter((item)=>item.dueDate> today)
+  }
+
+  const toDisplayableList = (list) => {
+     let res= list.map(item=>`${item.completed ? '[x] ':'[ ] '}${item.title} ${item.dueDate===today ? " ":item.dueDate}`).join("\n")
+        return res;
+  }
+  return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
+}
+
+// ####################################### #
+// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
+// ####################################### #
+module.exports = todoList;
