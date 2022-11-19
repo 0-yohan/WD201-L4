ff02999f46bdcd217d475af4a9f79f6a7e73b7d2
ameen <afor1m3en@gmail.com>
2022-11-19 16:49:59 +0530

    add: eslint & prettier

--- todo.js
+++ todo.js
@@ -1,30 +1,46 @@
+/* eslint-disable no-undef */
 const todoList = () => {
-  all = []
+  all = [];
   const add = (todoItem) => {
-    all.push(todoItem)
-  }
+    all.push(todoItem);
+  };
   const markAsComplete = (index) => {
-    all[index].completed = true
-  }
+    all[index].completed = true;
+  };
 
   const overdue = () => {
-   return all.filter((item)=>item.dueDate< today)
-  }
+    return all.filter((item) => item.dueDate < today);
+  };
 
   const dueToday = () => {
-     return all.filter((item)=>item.dueDate=== today)
-  }
+    return all.filter((item) => item.dueDate === today);
+  };
 
   const dueLater = () => {
-    return all.filter((item)=>item.dueDate> today)
-  }
+    return all.filter((item) => item.dueDate > today);
+  };
 
   const toDisplayableList = (list) => {
-     let res= list.map(item=>`${item.completed ? '[x] ':'[ ] '}${item.title} ${item.dueDate===today ? " ":item.dueDate}`).join("\n")
-        return res;
-  }
-  return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
-}
+    let res = list
+      .map(
+        (item) =>
+          `${item.completed ? "[x] " : "[ ] "}${item.title} ${
+            item.dueDate === today ? " " : item.dueDate
+          }`
+      )
+      .join("\n");
+    return res;
+  };
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
 
 // ####################################### #
 // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
